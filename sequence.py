a68db0ccc4e7a1c04718bc7e8fa96d1219ee0269
Barbara Vreede <b.vreede@gmail.com>
2019-07-25 11:41:47 +0200

    first commit

---
+++ sequence.py
@@ -0,0 +1,79 @@
+import pandas
+
+def sequencer(sequence,n):
+    global sequences
+    if n == 1:
+        sequence += 'S'
+        sequences.append(sequence)
+    else:
+        # duplicate
+        sequence1, sequence2 = sequence,sequence
+        n1, n2 = n,n
+        # alter
+        sequence1 += 'S'
+        sequence2 += 'D'
+        n1 -= 1
+        n2 -= 2
+        # proceed
+        if n1 > 0:
+            sequencer(sequence1,n1)
+        else:
+            sequences.append(sequence1)
+        if n2 > 0:
+            sequencer(sequence2,n2)
+        else:
+            sequences.append(sequence2)         
+
+def start(word):
+    n = len(word)
+    if n > 0:
+        sequencer('',n)
+    else:
+        sequences.append('')
+
+def makename(sequence,name):
+    namelist = []
+    index = 0
+    for i in sequence:
+        if i == 'D':
+            namelist.append(name[index:index+2])
+            index += 2
+        elif i == 'S':
+            namelist.append(name[index])
+            index += 1
+    return(namelist)
+
+## get the data and extract symbols
+ps = pandas.read_csv('periodicdata.csv')
+symbols = ps['symbol'].tolist()
+symlow = [s.lower() for s in symbols]
+
+def name_to_symbol(name):
+    sname = []
+    score = 0
+    for n in name:
+        n = n.lower()
+        if n in symlow:
+            i = symlow.index(n)
+            sym = symbols[i]
+            score += len(sym)
+        else:
+            sym = ''
+        sname.append(sym)
+    return(sname,score)
+
+
+
+names_to_test = ["Barbara"]
+for name in names_to_test:
+    sequences = []
+    start(name)
+    basescore = 0
+    periodicname = []
+    for s in sequences:
+        namelist = makename(s,name)
+        sname, score = name_to_symbol(namelist)
+        if score > basescore:
+            periodicname = sname
+            basescore = score
+    print("For the name", name, "the closest periodic table sequence is:", periodicname)

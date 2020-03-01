6a13c7e55278eb02d079c83cd4828cd2bd3d4519
Barbara Vreede <b.vreede@gmail.com>
2020-03-01 12:47:02 +0100

    restructured repo

---
+++ tests/test_periodicname.py
@@ -0,0 +1,16 @@
+import unittest
+import src.sequence as sequence
+
+
+class TestPeriodicNames(unittest.TestCase):
+    def test_makename(self):
+        # Test makename for a predictable outcome
+        self.assertEqual(sequence.makename('DSSDDDSS',"thanksrenato"),['th','a','n','ks','re','na','t','o'])
+        # Testing error cases in makename, including specific error message
+        self.assertRaisesRegex(ValueError, "Length of sequence", sequence.makename, "", "String")
+        self.assertRaisesRegex(ValueError, "Length of sequence", sequence.makename, "DDDDDDDDDSSSSDSDS", "String")
+
+
+
+if __name__ == '__main__':
+    unittest.main()

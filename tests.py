b1467d81d1ea7f626053229c70505c8572dc020e
Barbara Vreede <b.vreede@gmail.com>
2020-03-01 12:42:23 +0100

    restructured the repo

--- tests.py
+++ tests.py
@@ -1,14 +1,15 @@
 import unittest
-from sequence import makename
+import src.sequence as sequence
 
 
 class TestPeriodicNames(unittest.TestCase):
     def test_makename(self):
         # Test makename for a predictable outcome
-        self.assertEqual(makename('DSSDDDSS',"thanksrenato"),['th','a','n','ks','re','na','t','o'])
+        self.assertEqual(sequence.makename('DSSDDDSS',"thanksrenato"),['th','a','n','ks','re','na','t','o'])
         # Testing error cases in makename, including specific error message
-        self.assertRaisesRegex(ValueError, "Length of sequence", makename, "", "String")
-        self.assertRaisesRegex(ValueError, "Length of sequence", makename, "DDDDDDDDDSSSSDSDS", "String")
+        self.assertRaisesRegex(ValueError, "Length of sequence", sequence.makename, "", "String")
+        self.assertRaisesRegex(ValueError, "Length of sequence", sequence.makename, "DDDDDDDDDSSSSDSDS", "String")
+
 
 
 if __name__ == '__main__':

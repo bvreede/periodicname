360c18a9462ed1dac9926a054e8e0e5b5394f0aa
Barbara Vreede <b.vreede@gmail.com>
2020-03-14 11:02:41 +0100

    tests in context manager

--- tests/test_periodicname.py
+++ tests/test_periodicname.py
@@ -7,9 +7,12 @@ class TestPeriodicName(unittest.TestCase):
         # Test make_word_list for a predictable outcome
         sequence = pn.Sequencer("")
         self.assertEqual(sequence._make_word_list('DSSDDDSS',"thanksrenato"),['th','a','n','ks','re','na','t','o'])
+        
         # Testing error cases, including specific error message
-        self.assertRaisesRegex(ValueError, "Length of sequence", sequence._make_word_list, "", "String")
-        self.assertRaisesRegex(ValueError, "Length of sequence", sequence._make_word_list, "DDDDDDDDDSSSSDSDS", "String")
+        with self.assertRaisesRegex(ValueError, "Length of sequence"):
+            sequence._make_word_list("", "String")
+        with self.assertRaisesRegex(ValueError, "Length of sequence"):
+            sequence._make_word_list("DDDDDDDDDSSSSDSDS", "String")
 
 
 

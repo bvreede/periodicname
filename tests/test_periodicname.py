721a93d0f2a09e6c5652596601ae77fa9dd67c5c
Barbara Vreede <b.vreede@gmail.com>
2020-03-14 10:50:18 +0100

    instantiate class inside test function

--- tests/test_periodicname.py
+++ tests/test_periodicname.py
@@ -2,16 +2,14 @@ import unittest, os
 import periodicname.periodicname as pn
 
 
-
-
-
-class TestPeriodicNames(unittest.TestCase):
-    def test_makename(self):
-        # Test makename for a predictable outcome
-        self.assertEqual(pn._makename('DSSDDDSS',"thanksrenato"),['th','a','n','ks','re','na','t','o'])
-        # Testing error cases in makename, including specific error message
-        self.assertRaisesRegex(ValueError, "Length of sequence", pn._makename, "", "String")
-        self.assertRaisesRegex(ValueError, "Length of sequence", pn._makename, "DDDDDDDDDSSSSDSDS", "String")
+class TestPeriodicName(unittest.TestCase):
+    def test_make_word_list(self):
+        # Test make_word_list for a predictable outcome
+        sequence = pn.Sequencer("")
+        self.assertEqual(sequence._make_word_list('DSSDDDSS',"thanksrenato"),['th','a','n','ks','re','na','t','o'])
+        # Testing error cases, including specific error message
+        self.assertRaisesRegex(ValueError, "Length of sequence", sequence._make_word_list, "", "String")
+        self.assertRaisesRegex(ValueError, "Length of sequence", sequence._make_word_list, "DDDDDDDDDSSSSDSDS", "String")
 
 
 

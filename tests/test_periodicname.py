476ddc2857820d1ab36f098a99e7fb69236b58b8
Barbara Vreede <b.vreede@gmail.com>
2020-03-16 17:00:07 +0100

    add test on word_to_symbol

--- tests/test_periodicname.py
+++ tests/test_periodicname.py
@@ -3,16 +3,27 @@ import periodicname.periodicname as pn
 
 
 class TestPeriodicName(unittest.TestCase):
+    def setUp(self):
+        self.sequence = pn.Sequencer("testword")
+        self.periodic = pn.PeriodicElements()
+
     def test_make_word_list(self):
-        # Test make_word_list for a predictable outcome
-        sequence = pn.Sequencer("")
-        self.assertEqual(sequence._make_word_list('DSSDDDSS',"thanksrenato"),['th','a','n','ks','re','na','t','o'])
-        
+        """Testing make_word_list to correctly split a word."""
+        #self.sequence = pn.Sequencer("")
+        self.assertEqual(self.sequence._make_word_list('DSSDDDSS',"thanksrenato"),['th','a','n','ks','re','na','t','o'])
+
         # Testing error cases, including specific error message
         with self.assertRaisesRegex(ValueError, "Length of sequence"):
-            sequence._make_word_list("", "String")
+            self.sequence._make_word_list("", "String")
         with self.assertRaisesRegex(ValueError, "Length of sequence"):
-            sequence._make_word_list("DDDDDDDDDSSSSDSDS", "String")
+            self.sequence._make_word_list("DDDDDDDDDSSSSDSDS", "String")
+    
+    def test_word_to_symbol(self):
+        """Test word_to_symbol to assign"""
+        self.assertEqual(self.periodic.word_to_symbol(['B', 'Ar', 'Ba', 'Ra']),(['B', 'Ar', 'Ba', 'Ra'],7))
+
+
+
 
 
 

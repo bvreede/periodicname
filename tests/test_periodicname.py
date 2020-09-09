9856f1669b6642f0bf499d272e6c2fdbc127d301
Barbara Vreede <b.vreede@gmail.com>
2020-09-09 09:39:59 +0200

    Fix tests

--- tests/test_periodicname.py
+++ tests/test_periodicname.py
@@ -4,12 +4,10 @@ import periodicname.periodicname as pn
 
 class TestPeriodicName(unittest.TestCase):
     def setUp(self):
-        self.sequence = pn.Sequencer("testword")
-        self.periodic = pn.PeriodicElements()
+        self.sequence = pn.ElementalWord("testword")
 
     def test_make_word_list(self):
         """Testing make_word_list to correctly split a word."""
-        #self.sequence = pn.Sequencer("")
         self.assertEqual(self.sequence._make_word_list('DSSDDDSS',"thanksrenato"),['th','a','n','ks','re','na','t','o'])
 
         # Testing error cases, including specific error message
@@ -17,10 +15,11 @@ class TestPeriodicName(unittest.TestCase):
             self.sequence._make_word_list("", "String")
         with self.assertRaisesRegex(ValueError, "Length of sequence"):
             self.sequence._make_word_list("DDDDDDDDDSSSSDSDS", "String")
+
+    def test_periodic_name(self):
+        """Testing that the package correctly assigns elements to a sentence."""
+        self.assertEqual(pn.periodic_name("Hello World"),[['He', '', '', 'O'], ['W', 'O', '', '', '']])
     
-    def test_word_to_symbol(self):
-        """Test word_to_symbol to assign"""
-        self.assertEqual(self.periodic.word_to_symbol(['B', 'Ar', 'Ba', 'Ra']),(['B', 'Ar', 'Ba', 'Ra'],7))
 
 
 
